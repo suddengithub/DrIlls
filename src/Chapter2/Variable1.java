60f9203b0bcbd5924865078528a8fb391779e2d7
H.S <konyu1007@gmail.com>
2024-10-06 16:00:01 +0900

    Initial commit

---
+++ src/Chapter2/Variable1.java
@@ -0,0 +1,11 @@
+package Chapter2;
+
+public class Variable1 {
+    public static void main(String[] args) {
+        int level; // 정수 자료형인 level이라는 이름의 변수를 선언
+        level = 10; // 변수 level에 10을 대입한다
+        System.out.println("답 : " + level);
+
+
+    }
+}
---
+++ src/Chapter2/Variable2.java
@@ -0,0 +1,7 @@
+package Chapter2;
+
+public class Variable2 {
+    public static void main(String[] args) {
+        int level = 10; // 정수 자료형 변수 선언과 동시에 10을 대입(초기화 : 변수에 처음 값을 대입하는 것)
+    }
+}
